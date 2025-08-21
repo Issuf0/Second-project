649b565a3b0b8b6a09b5dde222bfde701afda5a2
Issuf0 <issufo.zuleca861@gmail.com>
2025-08-21 22:13:32 +0200

    Testando...

---
+++ src/data/civilCode.ts
@@ -0,0 +1,52 @@
+export const civilCode = [
+  {
+    id: '1',
+    number: 'Art. 1o',
+    text: 'Toda pessoa é capaz de direitos e deveres na ordem civil.',
+  },
+  {
+    id: '2',
+    number: 'Art. 2o',
+    text: 'A personalidade civil da pessoa começa do nascimento com vida; mas a lei põe a salvo, desde a concepção, os direitos do nascituro.',
+  },
+  {
+    id: '3',
+    number: 'Art. 3o',
+    text: 'São absolutamente incapazes de exercer pessoalmente os atos da vida civil os menores de 16 (dezesseis) anos.',
+  },
+  {
+    id: '4',
+    number: 'Art. 4o',
+    text: 'São incapazes, relativamente a certos atos ou à maneira de os exercer: (Redação dada pela Lei nº 13.146, de 2015) (Vigência)',
+  },
+  {
+    id: '5',
+    number: 'Art. 5o',
+    text: 'A menoridade cessa aos dezoito anos completos, quando a pessoa fica habilitada à prática de todos os atos da vida civil.',
+  },
+  {
+    id: '6',
+    number: 'Art. 6o',
+    text: 'A existência da pessoa natural termina com a morte; presume-se esta, quanto aos ausentes, nos casos em que a lei autoriza a abertura de sucessão definitiva.',
+  },
+  {
+    id: '7',
+    number: 'Art. 7o',
+    text: 'Pode ser declarada a morte presumida, sem decretação de ausência: I - se for extremamente provável a morte de quem estava em perigo de vida; II - se alguém, desaparecido em campanha ou feito prisioneiro, não for encontrado até dois anos após o término da guerra.',
+  },
+  {
+    id: '8',
+    number: 'Art. 8o',
+    text: 'Se dois ou mais indivíduos falecerem na mesma ocasião, não se podendo averiguar se algum dos comorientes precedeu aos outros, presumir-se-ão simultaneamente mortos.',
+  },
+  {
+    id: '9',
+    number: 'Art. 9o',
+    text: 'Serão registrados em registro público: I - os nascimentos, casamentos e óbitos; II - a emancipação por outorga dos pais ou por sentença do juiz; III - a interdição por incapacidade absoluta ou relativa; IV - a sentença declaratória de ausência e de morte presumida.',
+  },
+  {
+    id: '10',
+    number: 'Art. 10',
+    text: 'Far-se-á averbação em registro público: I - das sentenças que decretarem a nulidade ou anulação do casamento, o divórcio, a separação judicial e o restabelecimento da sociedade conjugal; II - dos atos judiciais ou extrajudiciais que declararem ou reconhecerem a filiação;',
+  },
+];
